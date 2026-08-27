1f4ba834093f2547723d7e7474e9eb68202376d8
Meena <meenapathma091@gmail.com>
2026-08-27 11:55:47 +0530

    mail

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://master-export-nine.vercel.app",
+        origin: ["https://master-export-nine.vercel.app",],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
